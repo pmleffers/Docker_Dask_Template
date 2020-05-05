da739d2aabdd564c089f88b157bff38d3f96d171
Pieter M Leffers <pmleffers@hotmail.com>
2020-05-04 19:28:08 -0700

    Add files via upload

---
+++ dask_projects/requirements.py
@@ -0,0 +1,14 @@
+import subprocess
+import sys
+
+print("This file installs the requirements for the project.")
+
+def install(package):
+    print('Installing '+package)
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+
+install_list=['graphviz','scipy','numpy==1.18.1','pandas==1.0.1','scikit-learn==0.22.*','python-git','matplotlib','seaborn','plotly','cytoolz','lz4','ipywidgets','dask-labextension==1.0.3','graphviz','gcsfs','pyarrow']
+
+for i in install_list:
+    install(i)
